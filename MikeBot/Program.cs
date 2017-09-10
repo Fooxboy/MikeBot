0e758309445b815cb9b2d59381fb9451c1eac39d
Славик Лис :3 <  :3>
2017-09-10 18:58:44 +0300

    Добавьте файлы проекта.

---
+++ MikeBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MikeBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
