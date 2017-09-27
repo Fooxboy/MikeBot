ccea5732c13811c0d856ea14b9dcaf8fabc7024c
Славик Лис <SGDKPUUPFI@outlook.com>
2017-09-27 08:25:08 +0300

    Дописан метод получения списка убитых  и их ролей.

--- MikeBot/Mafia/Logic/GetKillText.cs
+++ MikeBot/Mafia/Logic/GetKillText.cs
@@ -6,9 +6,29 @@ namespace MikeBot.Mafia.Logic
 {
     public static class GetKillText
     {
-        public static string Start(List<string> ids)
+        public static string Start(List<string> ids, string dialog_id)
         {
+            string response = "";
 
+            //Получаем id всех пользователей.
+            string id = "";
+
+            foreach(string s in ids)
+            {
+                id += $"{s},";
+            }
+
+            var obj = API.Method.Users.Get.Start(id).obj;
+
+           
+            for(int i=0; ids.Count< i; i++)
+            {
+                string character = Methods.GetCharactersFromId.Start(ids[i], dialog_id);
+
+                response += $"[{ids[i]}|{obj.response[i].first_name} {obj.response[i].last_name}] - Был {character}\n";
+            }
+
+            return response;
         }
     }
 }
