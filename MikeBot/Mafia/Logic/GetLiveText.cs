6941cec9994816b756d8375125716228167517c8
Славик Лис <SGDKPUUPFI@outlook.com>
2017-09-27 16:25:03 +0300

    Дописан класс для получения списка живых игроков.

--- MikeBot/Mafia/Logic/GetLiveText.cs
+++ MikeBot/Mafia/Logic/GetLiveText.cs
@@ -6,5 +6,23 @@ namespace MikeBot.Mafia.Logic
 {
     public static class GetLiveText
     {
+        public static string Start(List<string> users_id)
+        {
+            string users_ids = "";
+
+            foreach(string s in users_id)
+            {
+                users_ids += $"{s},";
+            }
+
+            var obj = API.Method.Users.Get.Start(users_ids);
+
+            string response = "";
+            for(int i = 0; users_id.Count <i; i++)
+            {
+                response += $"[{users_id[i]}|{obj.obj.response[i].first_name} {obj.obj.response[i].last_name}]\n";
+            }
+            return response;
+        }
     }
 }
