a99729efc0f50770b9774bc294e20b0eb8635bee
Славик Лис <SGDKPUUPFI@outlook.com>
2017-09-28 18:45:44 +0300

    Создана команда "Вылечить". Добавлнена логика Доктора.

---
+++ MikeBot/Mafia/Command/Treat.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MikeBot.Mafia.Command
+{
+    public static class Treat
+    {
+        public static void Start(string from, string id, string dialog_id)
+        {
+
+        }
+
+    }
+}
