520f47a36e14058d12631b0b43565c5cf4e93bd7
Славик Лис <SGDKPUUPFI@outlook.com>
2017-09-28 19:36:02 +0300

    Добавлена команда "узнать роль" для гадалки.

---
+++ MikeBot/Mafia/Command/Open_role.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MikeBot.Mafia.Command
+{
+    public static class Open_role
+    {
+        public static void Start(string from, string id, string  dialog_id)
+        {
+            var rand = new Random();
+
+            int value = rand.Next(1, 10);
+
+            bool ifOpen = false;
+
+            if(value == 3)
+            {
+                ifOpen = true;
+            }
+
+            if(ifOpen)
+            {
+                string character = Methods.GetCharactersFromId.Start(from,dialog_id);
+                Bot.API.Message.Send($"Скорее всего он - {character}", id);
+            } else
+            {
+                string text = "Ваших способностей не хватило! :( Вы не смогли узнать роль.";
+                Bot.API.Message.Send(text, id);
+            }
+        }
+    }
+}
