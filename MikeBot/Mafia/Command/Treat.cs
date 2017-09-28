c92258d76685d58a973bce768c5db87009f2f376
Славик Лис <SGDKPUUPFI@outlook.com>
2017-09-28 18:57:17 +0300

    Сделана команда "Вылечить".

--- MikeBot/Mafia/Command/Treat.cs
+++ MikeBot/Mafia/Command/Treat.cs
@@ -8,7 +8,14 @@ namespace MikeBot.Mafia.Command
     {
         public static void Start(string from, string id, string dialog_id)
         {
-
+            string character = Methods.GetCharactersFromId.Start(id, dialog_id);
+            if(character.ToLower() == "доктор")
+            {
+                Logic.Characters.Doctor.Start(from, id, dialog_id);
+            } else
+            {
+                Bot.API.Message.Send("Извините, Вам не доступна эта команда. Узнайте подробнее о вашей роли: Майк, мафия помощь <роль>.", id);
+            }
         }
 
     }
