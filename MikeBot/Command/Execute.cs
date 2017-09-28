1a0a75a102d45404a89d86b92b8df9612027e04b
Славик Лис <SGDKPUUPFI@outlook.com>
2017-09-28 16:36:10 +0300

    Создана команда "выполнить" Но она не работает.

---
+++ MikeBot/Command/Execute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.CodeAnalysis.CSharp;
+
+
+namespace MikeBot.Command
+{
+    public static class Execute
+    {
+        public static void Start(string[] arg)
+        {
+            string code = "";
+
+            foreach(string s in arg)
+            {
+                code += s;
+            }
+
+            
+
+        }
+    }
+}
